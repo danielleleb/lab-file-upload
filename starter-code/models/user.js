094ca2bd1c60e1e051d7c25c9bd2bc98110fda57
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-02-08 11:58:32 +0100

    initial set up done

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Picture = require('../models/picture').Picture
 
 const UserSchema = Schema({
   username: String,
-  email:    String,
+  email: String,
   password: String
-});
+  // picture: new Picture()
+})
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema)
 
-module.exports = User;
+module.exports = User
